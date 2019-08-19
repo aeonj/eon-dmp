6563cdefdfcec54c1a85dd3146aed0c8ba9112f5
cxj <aeonj@163.com>
2019-08-19 11:36:27 +0800

    上传路径初始化顺序问题

--- fap-core/src/main/java/eon/hg/fap/core/listener/SystemInitListener.java
+++ fap-core/src/main/java/eon/hg/fap/core/listener/SystemInitListener.java
@@ -1,7 +1,10 @@
 package eon.hg.fap.core.listener;
 
+import cn.hutool.core.util.StrUtil;
+import eon.hg.fap.common.properties.PropertiesBean;
 import eon.hg.fap.common.util.tools.FileHandler;
 import eon.hg.fap.core.constant.AeonConstants;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -13,11 +16,17 @@ import javax.servlet.annotation.WebListener;
  */
 @WebListener
 public class SystemInitListener implements ServletContextListener {
+    @Autowired
+    PropertiesBean propertiesBean;
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
-            String path = FileHandler.getLicenseFilePath();
+
+            String path = PropertiesBean.UPLOAD_FOLDER;
+            if (StrUtil.isBlank(path)) {
+                path = FileHandler.getLicenseFilePath();
+            }
             //String path = ResourceUtils.getURL("classpath:").getPath();
             AeonConstants.VLicense.install(path);
         }catch (Exception e) {
