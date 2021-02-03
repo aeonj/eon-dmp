3a9ed431a79524a953ae4a41fb89b8aed79e7f0d
cxj <aeonj@163.com>
2021-02-03 17:23:07 +0800

    maven引用调整，增加扩展属性

--- fap-common/src/main/java/eon/hg/fap/common/properties/ExternalProperties.java
+++ fap-common/src/main/java/eon/hg/fap/common/properties/ExternalProperties.java
@@ -1,10 +1,19 @@
 package eon.hg.fap.common.properties;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/**
+ * 平台扩展属性
+ */
 @ConfigurationProperties(prefix = "eon.hg.external")
 @Data
 public class ExternalProperties {
+    //扩展的待初始化的实体对象包路径
     private String entity_packages;
+    //慢请求处理时间阀值
+    private long slow_req_millis = 10000;
+    //记载用户请求信息的对象
+    private String user_context_class = "eon.hg.fap.support.session.UserContext";
 }
--- fap-core/src/main/java/eon/hg/fap/support/session/UserContext.java
+++ fap-core/src/main/java/eon/hg/fap/support/session/UserContext.java
@@ -1,15 +1,68 @@
 package eon.hg.fap.support.session;
 
+import eon.hg.fap.db.model.virtual.OnlineUser;
+
 import java.io.Serializable;
+import java.util.HashMap;
 
 /**
  * 当前登录的用户对象模型
  */
 public class UserContext implements Serializable,Cloneable {
 
-    private String userid;
-    private String username;
-    private String nickname;
+    private OnlineUser onlineUser;
     private Long menuId;
+    private Integer year;
+    // 用户其他信息的HashMap
+    protected HashMap context = new HashMap();
+
+    public OnlineUser getOnlineUser() {
+        return onlineUser;
+    }
+
+    public void setOnlineUser(OnlineUser onlineUser) {
+        this.onlineUser = onlineUser;
+    }
+
+    public Long getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(Long menuId) {
+        this.menuId = menuId;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public void setAttribute(Object attrKey, Object attrValue) {
+        if (context.containsKey(attrKey)) {
+            context.remove(attrKey);
+            context.put(attrKey, attrValue);
+        } else {
+            context.put(attrKey, attrValue);
+        }
+    }
+
+    public Object getAttribute(Object attrKey) {
+        if (context.containsKey(attrKey)) {
+            return context.get(attrKey);
+        } else
+            return null;
+    }
+
+    public HashMap getContext() {
+        return context;
+    }
+
+    public void setContext(HashMap context) {
+        this.context = context;
+        //System.out.println("set UserInfoContext:"+context.get("user_id"));
+    }
 
 }
