dc988292a4af1c2aeabe6cca64df485904372224
cxj <aeonj@163.com>
2021-07-02 17:26:00 +0800

    异常断言统一返回ResultException

--- fap-core/src/main/java/eon/hg/fap/core/exception/Assert.java
+++ fap-core/src/main/java/eon/hg/fap/core/exception/Assert.java
@@ -7,28 +7,28 @@ import java.util.List;
 public abstract class Assert {
     public static void notEmpty(long[] ids, String exceptionName){
         if(ids == null || ids.length < 1)
-            throw new RuntimeException(exceptionName);
+            throw new ResultException(exceptionName);
     }
 
     public static void notEmpty(List list, String exceptionName){
         if(list == null || list.isEmpty())
-            throw new RuntimeException(exceptionName);
+            throw new ResultException(exceptionName);
     }
 
     public static void notNull(Object data, String exceptionName){
         if(data == null)
-            throw new RuntimeException(exceptionName);
+            throw new ResultException(exceptionName);
     }
 
     public static void notEmpty(String string, String exceptionMessage){
         if (CommUtil.isEmpty(string)){
-            throw new RuntimeException(exceptionMessage);
+            throw new ResultException(exceptionMessage);
         }
     }
 
     public static void isInstandOf(Class<?> type, Object obj, String exceptionMessage) {
         if (false == type.isInstance(obj)){
-            throw new RuntimeException(exceptionMessage);
+            throw new ResultException(exceptionMessage);
         }
     }
 }
